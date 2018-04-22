69c6730c792061d4115ef1ae809aa70853071a3a
Jonas Molina Ramirez <jonas.molinaramirez@stud.tu-darmstadt.de>
2018-04-22 09:44:17 +0200

    Added README part to pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# README
+#
+# The script expects three parameters ordered as listed:
+# <input_dir>	Input directory: a directory containing *.class files (also in subfolders,
+# 	e.g. unpacked JARs)
+# <output_dir>	Name for new output directory: the name of the newly created input directory
+# 	The directory will be create relative to the provided input directory
+# <dataset>	Name for new dataset: a name for the newly created dataset. Output data will
+#		be stored in a subdirectory of the output directory named like the dataset
+#
+# How to run the script:
+# ./pipeline.sh <input_dir> <output_dir> <dataset>
+# Example:
+# ./pipeline.sh $SEML_DATA/examples/test/ examples_out my_dataset
+# Hint:
+# Although $SEML_DATA is not necessary to run the script, it would be helpful to
+# create a dedicated directory and store its path as an environment variable.
+# Throughout multiple runs of the script with different datasets and possibly
+# different output folders, $SEML_DATA will look as follows:
+# $SEML_DATA/
+#	- <input_dir_1>/
+#		-	<dataset_a>/
+#		-	<dataset_b>/
+# - <input_dir_2>/
+#		-	...
+# - <input_dir_n>/
+#		-	...
+#	- <output_dir_1>/
+#		-	<dataset_c>/
+#		-	<dataset_d>/
+# - <output_dir_2>/
+#		-	...
+# - <output_dir_m>/
+#		-	...
+
+
 function read_principal_components(){
 	echo "Enter number of principal components:"
 	read principal_components
@@ -10,13 +46,13 @@ function read_principal_components(){
 		echo "Using $principal_components principal components"
 	fi
 }
- 
+
 ### ### #### ### ###
 # RUN GRAPHBUILDER #
 ### ### #### ### ###
 
 function graphbuilder(){
-	cd "$SEML/code/graphbuilder/" 
+	cd "$SEML/code/graphbuilder/"
 	# Run ram.sh to set JAVA_OPTS environment variable
 	source "./ram.sh"
 	# Example call:
@@ -45,7 +81,7 @@ function kernel_pca(){
 	time python2 kernel_pca.py -ds "$dataset" -b "$SEML_DATA/$output_dir/" -c "$principal_components"
 }
 
-### #### ### ### ### ### ### 
+### #### ### ### ### ### ###
 # RUN SPECTRAL CLUSTERING  #
 ### #### ### ### ### ### ###
 function spectral_clustering() {
@@ -54,7 +90,7 @@ function spectral_clustering() {
 	read_principal_components
 	time python2 spectral_clustering.py -ds "$dataset" -b "$SEML_DATA/$output_dir/" -c $principal_components
 }
-### #### ### 
+### #### ###
 # RUN SVM  #
 ### #### ###
 function svm() {
@@ -79,9 +115,9 @@ echo "[6] all of the above"
 read selection
 case $selection in
 	[1]*)
-		graphbuilder	
+		graphbuilder
 		;;
-	
+
 	[2]*)
 		hgk
 		;;
