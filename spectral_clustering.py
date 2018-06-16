25c58ec461d56d86d67e4d2261270aae8a7651d5
Jonas Molina Ramirez <jonas.molinaramirez@stud.tu-darmstadt.de>
2018-06-16 14:15:23 +0200

    Added silhouette metrics to spectral_clustering

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -20,17 +20,42 @@ def main():
 	ds_name = args.dataset
 	prefix = ds_path + ds_name + '/' + ds_name
 	components = args.components
-	
+
 	readCsv = lambda path : np.genfromtxt(path, delimiter=',')
 	filePath = prefix + "_kpca-" + str(components) +".csv"
 	print("Reading: " + filePath)
 	X_kpca = readCsv(filePath)
 	print(X_kpca)
 
-
+	silhouette_avgs = []
 	#subsampled_indices = np.random.choice(X_kpca.shape[0],200,replace=False)
+	for components in range(2,15):
+		print("Number of components: %d" %components)
+		spectral_labels = compute_spectral_clusters(X_kpca, components)
 
-	spectral = SpectralClustering(n_clusters=3,eigen_solver='arpack')#,affinity="nearest_neighbors")
+		silhouette_avg, sample_silhouette_labels = compute_silhouette_metrics(X_kpca,spectral_labels)
+		silhouette_avgs.append(silhouette_avg)
+
+		print("Silhouette Average:")
+		print(silhouette_avg)
+		print("Silhouette Sample Labels:")
+		print(sample_silhouette_labels)
+		print
+
+	print("Max silhouette_avg: %f for component %d" % (np.max(silhouette_avgs),np.argmax(silhouette_avgs)+2))
+	with open(prefix + "_labels-" + str(components) + ".csv","w") as f:
+		for label in spectral_labels:
+			f.write( str(label) + '\n')
+
+def compute_silhouette_metrics(X_kpca,spectral_labels):
+	# compute silhouette scores
+	import sklearn.metrics as skm
+	silhouette_avg = skm.silhouette_score(X_kpca,spectral_labels)
+	sample_silhouette_labels = skm.silhouette_samples(X_kpca,spectral_labels)
+	return silhouette_avg, sample_silhouette_labels
+
+def compute_spectral_clusters(X_kpca,components):
+	spectral = SpectralClustering(n_clusters=components,eigen_solver='arpack')#,affinity="nearest_neighbors")
 	spectral_labels = spectral.fit_predict(X_kpca)#[subsampled_indices])
 	N = spectral_labels.shape[0]
 	uni  = np.unique(spectral_labels)
@@ -40,11 +65,7 @@ def main():
 		class_dist.append((i,prior))
 		print("Class %d: %f" % (i,prior))
 	print(class_dist)
-
-
-	with open(prefix + "_labels-" + str(components) + ".csv","w") as f:
-		for label in spectral_labels:
-			f.write( str(label) + '\n')
+	return spectral_labels
 
 if __name__ == "__main__":
 	main()
